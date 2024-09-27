703040f8edf49570cf79c6d1351213e59104a1d7
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-09-26 23:22:54 -0400

    added checkout session

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,55 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import Stripe from 'stripe';
+import { HTTPException } from 'hono/http-exception';
 import "dotenv/config";
 
 // Strip Instantiation
-import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
-
-
 
 
 // Routes for Web API
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/success', (c) => {
+  return c.text('Success!')
 })
 
-app.post('/', (c) => {
-  return c.text('Post Request')
+app.get('/cancel', (c) => {
+  return c.text('Canceled!')
 })
 
+// Hardcoded Product Test
+// Callback with context argument
+app.post('/checkout', async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'price_1Q3UMgHHxosx5eaLBLMokL6T',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    });
+
+    return c.json(session);
+  } catch (error: any) {
+    console.error(error);
+    throw new HTTPException(500, { message: error?.message });
+  }
+});
+
+
+
+
+// Start the Node Server with the Hono Web Framework
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
-// Start the Node Server with the Hono Web Framework
 serve({
   fetch: app.fetch,
   port
