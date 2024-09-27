5cb76fb75d916cf16577a0f9014e7551060b19e1
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-09-26 22:38:42 -0400

    imported .env variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import "dotenv/config";
 
+// Strip Instantiation
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+
+
+
+
+// Routes for Web API
 const app = new Hono()
 
 app.get('/', (c) => {
@@ -14,6 +24,7 @@ app.post('/', (c) => {
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
+// Start the Node Server with the Hono Web Framework
 serve({
   fetch: app.fetch,
   port
